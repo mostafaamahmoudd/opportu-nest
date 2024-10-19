39b55f4ae71fd118dfd247d262a01b620eb79958
Mostafa <mostafamahmoud805554@std.mans.edu.eg>
2024-10-19 17:12:46 +0300

    final project

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,18 @@ export default {
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
-        './resources/**/*.vue',
     ],
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            colors: {
+                "black": "#060606"
             },
+            fontFamily: {
+                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"]
+            },
+            fontSize: {
+                "2xs": ".625rem" // 10px
+            }
         },
     },
     plugins: [],
